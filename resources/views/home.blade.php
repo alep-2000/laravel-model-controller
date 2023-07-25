e918dc520d7b222724c3b4fdfec0976e1157cb8e
alep-2000 <alessiopiras056@gmail.com>
2023-07-25 15:30:12 +0200

    Add yield and section

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,6 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+@extends('layouts.app')
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title>Laravel</title>
-
-    <!-- Fonts -->
-    <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-
-    <!-- Styles -->
-    @vite('resources/js/app.js')
-
-</head>
-
-<body>
-
-    <main class="bg-light">
+@section('content')
         <div class="container">
             <div class="row">
                 <div class="col-12">
@@ -40,8 +23,4 @@
             </div>
 
         </div>
-    </main>
-
-</body>
-
-</html>
+@endsection
