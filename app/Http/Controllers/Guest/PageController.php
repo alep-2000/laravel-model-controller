6ee523e03a57885573392f7101ffd6da13e79b13
alep-2000 <alessiopiras056@gmail.com>
2023-07-25 16:15:42 +0200

    Exercise finisched

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -4,11 +4,12 @@ namespace App\Http\Controllers\Guest;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\Models\Comic;
+use App\Models\Comics;
 
 class PageController extends Controller
 {
     public function index(){
-        return view('home');
+        $comics = Comics::all();
+        return view('home', compact('comics'));
     }
 }
